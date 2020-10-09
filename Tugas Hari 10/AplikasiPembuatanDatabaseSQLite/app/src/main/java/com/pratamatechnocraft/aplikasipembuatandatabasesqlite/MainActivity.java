9d87f8731be9196f80de63822f1171951d11c525
Vyan-LaptopAsus <vyanaryprabowo9763@gmail.com>
2020-10-09 23:31:56 +0700

    Tugas Hari Ke 10

---
+++ Tugas Hari 10/AplikasiPembuatanDatabaseSQLite/app/src/main/java/com/pratamatechnocraft/aplikasipembuatandatabasesqlite/MainActivity.java
@@ -0,0 +1,53 @@
+package com.pratamatechnocraft.aplikasipembuatandatabasesqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnStore, btnGetall;
+    private EditText etname;
+    private DatabaseHelper databaseHelper;
+    private TextView tvnames;
+    private ArrayList<String> arrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(this);
+        tvnames = (TextView) findViewById(R.id.tvnames);
+        btnStore = (Button) findViewById(R.id.btnstore);
+        btnGetall = (Button) findViewById(R.id.btnget);
+        etname = (EditText) findViewById(R.id.etname);
+
+        btnStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                databaseHelper.addStudentDetail(etname.getText().toString());
+                etname.setText("");
+                Toast.makeText(MainActivity.this,"Stored Successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnGetall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                arrayList = databaseHelper.getAllStudentsList();
+                tvnames.setText("");
+                for (int i = 0; i < arrayList.size(); i++){
+                    tvnames.setText(tvnames.getText().toString()+", "+arrayList.get(i));
+                }
+            }
+        });
+    }
+}
